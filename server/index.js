7a969d055a6dfb6ca7a4c067b0e2f2b17e5055a2
June Zhao <bz.bijun@gmail.com>
2023-10-03 13:25:21 -0700

    Connect Mongoose first

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql"); // CommonJS
 const schema = require("./schema/schema");
-const port = process.env.PORT || 5000;
 const colors = require("colors");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 
-connectDB();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
@@ -20,4 +20,18 @@ app.use(
   })
 );
 
-app.listen(port, console.log(`Server running on port ${port}`));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongodb connected");
+    app.listen(
+      PORT,
+      console.log(
+        `Server running in ${process.env.NODE_ENV} mode on ${process.env.PORT}`
+      )
+    );
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
